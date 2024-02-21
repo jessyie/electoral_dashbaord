3197631de356ad6ac78612b19685a1a2146b813a
Jesse Kwame Ahado <jessedabankah@gmail.com>
2024-02-21 14:50:38 +0000

    deploying domain_E

--- geospatialproject/settings.py
+++ geospatialproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", config('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", config('DEBUG'))
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com','www.election-gh.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'http://0.0.0.0:10000', 'localhost', '.onrender.com','www.election-gh.com']
 #ALLOWED_HOSTS = ['0.0.0.0:10000', '.onrender.com','www.election-gh.com']
 #ALLOWED_HOSTS = ['0.0.0.0:10000', 'localhost', 'electoral-dashboard-ghana.onrender.com']
 
